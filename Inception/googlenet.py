efcae1d391746efcf2224882586b998a9c38494d
Brian McClannahan <b523m844@ku.edu>
2019-08-15 11:49:17 -0500

    Switched input size back to 224 from 112

--- Inception/googlenet.py
+++ Inception/googlenet.py
@@ -139,7 +139,7 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained = 
         set_parameter_requires_grad(model_ft, feature_extract)
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs, num_classes)
-        input_size = 112
+        input_size = 224
 
      return model_ft, input_size
 
