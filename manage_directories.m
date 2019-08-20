f9d71e9d045f554db422d27c0754e3ed1b2d7391
Brian McClannahan <b523m844@ku.edu>
2019-08-20 13:24:13 -0500

    Changed number of epochs for testing

--- manage_directories.m
+++ manage_directories.m
@@ -1,5 +1,5 @@
 function manage_directories(data_set_folder,training_set_folder)
-build_data_set(data_set_folder,1000)
+build_data_set(data_set_folder,1)
 files = dir('*.jpg');
 for i = 1:length(files)
     file = files(i).name;
