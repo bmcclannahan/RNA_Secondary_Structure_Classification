b1e0c7d7a89b2551d8019636d8bd4dbce0b1f80a
Brian McClannahan <b523m844@ku.edu>
2019-08-20 13:54:17 -0500

    Removed unnecessary debugging.

--- build_data_set.m
+++ build_data_set.m
@@ -5,15 +5,15 @@ keys = map.keys;
 for e = 1:num_epochs
     rn = uint16(rand()*num_keys);
     key = cell2mat(keys(rn));
-    family = key(1)
+    family = key(1);
     rn = uint16(rand()*(length(map(family))-1)+1);
     family_arr = map(family);
     file1 = family_arr(rn);
     previous = [rn,0,0,0,0];
     for i = 2:5
-        rn = uint16(rand()*length(map(family)))
+        rn = uint16(rand()*length(map(family)));
         while ismember(rn,previous)
-            rn = uint16(rand()*(length(map(family))-1)+1)
+            rn = uint16(rand()*(length(map(family))-1)+1);
         end
         previous(i) = rn;
         save_image(folder_location,file1, family_arr(rn));
@@ -24,7 +24,7 @@ for e = 1:num_epochs
             rn = uint16(rand()*(length(map(family))-1)+1);
         end
         key2 = cell2mat(keys(rn));
-        family2 = key2
+        family2 = key2;
         rn = uint16(rand()*(length(map(family2))-1)+1);
         family_arr2 = map(family2);
         save_image(folder_location,file1, family_arr2(rn));
--- manage_directories.m
+++ manage_directories.m
@@ -1,12 +1,12 @@
 function manage_directories(data_set_folder,training_set_folder)
-p = 'Building Data Set'
-build_data_set(data_set_folder,1)
-p = 'Dataset built, moving files'
+disp('Building Data Set')
+build_data_set(data_set_folder,3000)
+disp('Dataset built, moving files')
 files = dir('*.jpg');
 num_images = length(files)
 for i = 1:length(files)
     file = files(i).name;
     movefile(file, training_set_folder);
 end
-p = 'Files moved.'
+disp('Files moved.')
 end
