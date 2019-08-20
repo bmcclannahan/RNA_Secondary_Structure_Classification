68f3db79c35e19290bef5d72e7efe97b15b66c7c
Brian McClannahan <b523m844@ku.edu>
2019-08-20 13:51:59 -0500

    More debug stuff.

--- manage_directories.m
+++ manage_directories.m
@@ -1,11 +1,12 @@
 function manage_directories(data_set_folder,training_set_folder)
-disp('Building Data Set')
+p = 'Building Data Set'
 build_data_set(data_set_folder,1)
-disp('Dataset built, moving files')
+p = 'Dataset built, moving files'
 files = dir('*.jpg');
+num_images = length(files)
 for i = 1:length(files)
     file = files(i).name;
     movefile(file, training_set_folder);
 end
-disp('Files moved.')
+p = 'Files moved.'
 end
