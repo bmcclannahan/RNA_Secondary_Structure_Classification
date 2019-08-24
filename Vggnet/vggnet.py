ae284f66d5713c9f245a4a64a62f2d67df77fb8d
Brian McClannahan <b523m844@ku.edu>
2019-08-24 17:08:54 -0500

    Changed vggnet to use the big training set.

--- Vggnet/vggnet.py
+++ Vggnet/vggnet.py
@@ -17,7 +17,7 @@ print("PyTorch Version: ", torch.__version__)
 print("Torchvision Version: ", torchvision.__version__)
 
 
-data_dir = "/scratch/b523m844/RNA_Secondary_Structure_Classification/Small_Training_Set"
+data_dir = "/scratch/b523m844/RNA_Secondary_Structure_Classification/Big_Training_Set"
 
 model_name = "vggnet"
 
