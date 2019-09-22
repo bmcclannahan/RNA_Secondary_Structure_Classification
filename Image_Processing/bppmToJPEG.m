c3dbb2ee18445aace9e90052a5e906f4cbd5e29c
Brian McClannahan <b523m844@ku.edu>
2019-09-22 18:31:21 -0500

    Added file to convert bppm files to jpeg images.

---
+++ Image_Processing/bppmToJPEG.m
@@ -0,0 +1,28 @@
+function A = bppmToJPEG(dirName)
+
+%dirName directory from which .bppm files have to be read and respective .jpg files will be stored...
+ if 7==exist(dirName,'dir')
+ 
+ %Files=getAllFiles(dirName, '.bppm', true);%%dir(dirName,'*.bppm');
+ files = dir(fullfile(dirName, '*.bppm'));
+
+L = length(files);
+
+for i=1:L
+
+file=files(i).name;
+
+filepath = fullfile( dirName, file );
+
+[pathstr, name, ext] = fileparts(filepath);
+
+A = dlmread(filepath);
+normA = A - min(A(:));
+normA = (normA ./ max(normA(:)));
+imwrite(normA,[pathstr,'\',name,'.jpg']);
+
+end
+
+ else
+ error('Error: No .bppm file found at given directory. Please try again...');
+ end;
