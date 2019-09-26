9abb7fb543b13e86df8f84d169f49704f7f993fb
Brian McClannahan <b523m844@ku.edu>
2019-09-26 13:26:27 -0500

    Changed dir function.

--- Image_Processing/bppmToJPEG.m
+++ Image_Processing/bppmToJPEG.m
@@ -1,7 +1,7 @@
 function A = bppmToJPEG(dirName)
 
 %dirName directory from which .bppm files have to be read and respective .jpg files will be stored...
-    if 7==exist(dirName,'dir')
+%     if 7==exist(dirName,'dir')
 
      %Files=getAllFiles(dirName, '.bppm', true);%%dir(dirName,'*.bppm');
         files = dir(dirName + '*.bppm');
@@ -23,7 +23,7 @@ function A = bppmToJPEG(dirName)
 
         end
 
-    else
-        error('Error: No .bppm file found at given directory. Please try again...');
-    end
+%     else
+%         error('Error: No .bppm file found at given directory. Please try again...');
+%     end
 end
