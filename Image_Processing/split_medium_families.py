ed6f9203afe0eb69950fa850ee87ec7d677b2b95
Brian McClannahan <b523m844@ku.edu>
2019-10-02 13:34:00 -0500

    Added correct file this time.

---
+++ Image_Processing/split_medium_families.py
@@ -0,0 +1,47 @@
+import os, glob
+from random import random
+
+seed_data = "/users/b523m844/RNA_Secondary_Structure_Classification/seed_info/Medium_families.txt"
+source_dir = "/scratch/b523m844/RNA_Secondary_Structure_Classification/Full_Image_Set/Medium_Families/"
+
+train = .7
+val = .1
+test = .2
+
+train_families = []
+val_families = []
+test_families = []
+
+with open(seed_data) as f:
+    medium_families = f.readlines()
+medium_families = [x.rstrip() for x in medium_families]
+
+for fam in medium_families:
+    split = fam.split(",")
+    family = int(split[0][2:])
+    rand = random()
+    if rand < train:
+        train_families.append(family)
+    elif rand < train + val:
+        val_families.append(family)
+    else:
+        test_families.append(family)
+
+files = glob.glob(source_dir+"*.jpg")
+
+for f in files:
+    split = f.split('/')
+    family_name = split[-1][:7]
+    family = int(family_name[2:])
+    folder = ""
+    if family in train_families:
+        folder = "train"
+    elif family in val_families:
+        folder = "val"
+    elif family in test_families:
+        folder = "test"
+    if folder != "":
+        split.insert(-1,folder)
+        new_file = "/".join(split)
+        print(f, new_file)
+        #os.rename(f,new_file)
