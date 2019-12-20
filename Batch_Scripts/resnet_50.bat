96db96819496ac751559175dbe82dd2c4cb06fad
Brian McClannahan <b523m844@ku.edu>
2019-12-19 23:18:38 -0600

    Slurm jobs can't use variables in python calls so they have been changed.

--- Batch_Scripts/resnet_50.bat
+++ Batch_Scripts/resnet_50.bat
@@ -22,7 +22,7 @@ source activate
 rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt
 rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt
 
-python ~/RNA_Secondary_Structure_Classification/models/$model.py
+python ~/RNA_Secondary_Structure_Classification/models/resnet_50.py
 
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_train_loss.txt
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_val_loss.txt
