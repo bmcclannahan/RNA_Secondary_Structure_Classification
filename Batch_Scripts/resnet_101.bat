6abb641b43bc175cdeec40d467043dfa767883e0
Brian McClannahan <b523m844@ku.edu>
2019-12-19 23:10:44 -0600

    Updated batch scripts.

--- Batch_Scripts/resnet_101.bat
+++ Batch_Scripts/resnet_101.bat
@@ -22,9 +22,9 @@ model="resnet_101"
 rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt
 rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt
 
-python ~/RNA_Secondary_Structure_Classification/models/resnet_101.py
+python ~/RNA_Secondary_Structure_Classification/models/$model.py
 
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_train_loss.txt
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_val_loss.txt
 
-python ~/RNA_Secondary_Structure_Classification/models/resnet_101_test.py
+python ~/RNA_Secondary_Structure_Classification/models/$model_test.py
