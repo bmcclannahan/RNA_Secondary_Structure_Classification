48644e03a35e7942c84714a8b6121358554b00ed
Brian McClannahan <b523m844@ku.edu>
2019-12-19 23:13:35 -0600

    Fixed naming problem in resnet 101 model and changed batch scripts to use literal name for test calls.

--- Batch_Scripts/resnet_101.bat
+++ Batch_Scripts/resnet_101.bat
@@ -27,4 +27,4 @@ python ~/RNA_Secondary_Structure_Classification/models/$model.py
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_train_loss.txt
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_val_loss.txt
 
-python ~/RNA_Secondary_Structure_Classification/models/$model_test.py
+python ~/RNA_Secondary_Structure_Classification/models/resnet_101_test.py
