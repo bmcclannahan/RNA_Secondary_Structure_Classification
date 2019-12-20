dab8a4e89114893e21f1cc00ae1438a986affb36
Brian McClannahan <b523m844@ku.edu>
2019-12-20 11:07:44 -0600

    Fixed another variable problem with slurm.

--- Batch_Scripts/resnet_50.bat
+++ Batch_Scripts/resnet_50.bat
@@ -24,7 +24,7 @@ rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt
 
 python ~/RNA_Secondary_Structure_Classification/models/resnet_50.py
 
-cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_train_loss.txt
-cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_val_loss.txt
+cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/resnet_50_train_loss.txt
+cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/resnet_50_val_loss.txt
 
 python ~/RNA_Secondary_Structure_Classification/models/resnet_50_test.py
--- Batch_Scripts/vgg_19.bat
+++ Batch_Scripts/vgg_19.bat
@@ -24,7 +24,7 @@ rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt
 
 python ~/RNA_Secondary_Structure_Classification/models/vgg_19.py
 
-cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_train_loss.txt
-cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/$model_val_loss.txt
+cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/vgg_19_train_loss.txt
+cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/vgg_19_val_loss.txt
 
 python ~/RNA_Secondary_Structure_Classification/models/vgg_19_test.py
