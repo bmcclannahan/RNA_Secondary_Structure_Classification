1c5f10600b21a5f0ba4b18de4c79e10a17c3362e
Brian McClannahan <b523m844@ku.edu>
2019-12-21 17:57:25 -0600

    Removed test call after moving it into the train/val python file.

--- Batch_Scripts/resnet_101.bat
+++ Batch_Scripts/resnet_101.bat
@@ -26,5 +26,3 @@ python ~/RNA_Secondary_Structure_Classification/models/resnet_101.py
 
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/resnet_101_train_loss.txt
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/resnet_101_val_loss.txt
-
-python ~/RNA_Secondary_Structure_Classification/models/resnet_101_test.py
--- Batch_Scripts/vgg_19.bat
+++ Batch_Scripts/vgg_19.bat
@@ -26,5 +26,3 @@ python ~/RNA_Secondary_Structure_Classification/models/vgg_19.py
 
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/vgg_19_train_loss.txt
 cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/vgg_19_val_loss.txt
-
-python ~/RNA_Secondary_Structure_Classification/models/vgg_19_test.py
