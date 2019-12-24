f2a500df45f42db378f08237b5dc09a2c631530d
Brian McClannahan <b523m844@ku.edu>
2019-12-23 20:33:48 -0600

    Created new file for choosing smaller dataset from current dataset.

---
+++ Image_Processing/randomly_select_training_images.py
@@ -0,0 +1,41 @@
+from glob import glob
+from random import randint
+
+file_dir = "/data/rna_classification/train/Diff_Family/"
+file_list = "/data/rna_classifcation/training_file_list.txt"
+training_list = open(file_list,'w')
+files = glob(file_dir+"*.jpg")
+num_per_family = 20
+
+family_dict = dict()
+
+for f in files:
+    family1,family2,img1,img2,_ = f.split('_')
+    if family1 not in family_dict.keys():
+        family_dict[family1] = [img1]
+    elif img1 not in family_dict[family1]:
+        family_dict[family1].append(img1)
+    if family2 not in family_dict.keys():
+        family_dict[family2] = [img2]
+    elif img2 not in family_dict[family2]:
+        family_dict[family2].append(img2)
+    
+keys = family_dict.keys()
+
+for key in keys:
+    imgs = family_dict[key]
+    at_key = 0 #so I can add 1 to i when I hit the index of the current family to prevent making same class images
+    for i in len(keys)-1:
+        if keys[i] == key:
+            at_key = 1
+        r1 = randint(0,len(imgs)-1)
+        img1 = imgs[r1]
+        r2 = randint(0,len(keys))
+        while keys[r2] == key:
+            r2 = randint(0,len(keys)-1)
+        family2 = keys[r2]
+        imgs2 = family_dict[key]
+        r3 = randint(0,len(imgs2)-1)
+        img2 = imgs2[r3]
+        file_name = '_'.join([str(key),str(family2),str(img1),str(img2),"0.jpg"])
+        training_list.write(file_name + '\n')
