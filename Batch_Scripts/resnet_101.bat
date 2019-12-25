8e63ba8d56cbebeb166918736cdb2269276eb22d
Brian McClannahan <b523m844@ku.edu>
2019-12-25 11:38:42 -0600

    Revert "Removed old, unnecessary batch scripts."
    This reverts commit 39f5e83c7635d2bf9dced4c48f479151766af74f.

---
+++ Batch_Scripts/resnet_101.bat
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH --mem=16GB
+#SBATCH -p gpu
+#SBATCH --gres="gpu:k40:1"
+#SBATCH -J RNA-Resnet50
+
+module load Python/3.5.2
+module load slurm-torque/14.11.8
+module load CUDA/9.0.176
+module load cuDNN/7-CUDA-9.0.176
+
+cd /nfs/apps/7/arch/generic/Python/3.5.2/bin
+pyvenv /scratch/b523m844/python352/bin/
+cd /scratch/b523m844/python352/bin/
+source activate
+
+model="resnet_101"
+
+rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt
+rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt
+
+python ~/RNA_Secondary_Structure_Classification/models/resnet_101.py
+
+cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/resnet_101_train_loss.txt
+cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/resnet_101_val_loss.txt
---
+++ Batch_Scripts/vgg_19.bat
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH --mem=16GB
+#SBATCH -p gpu
+#SBATCH --gres="gpu:k40:1"
+#SBATCH -J RNA-Resnet50
+
+model="vgg_19"
+
+module load Python/3.5.2
+module load slurm-torque/14.11.8
+module load CUDA/9.0.176
+module load cuDNN/7-CUDA-9.0.176
+
+cd /nfs/apps/7/arch/generic/Python/3.5.2/bin
+pyvenv /scratch/b523m844/python352/bin/
+cd /scratch/b523m844/python352/bin/
+source activate
+
+rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt
+rm /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt
+
+python ~/RNA_Secondary_Structure_Classification/models/vgg_19.py
+
+cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/train_loss.txt ~/RNA_Secondary_Structure_Classification/utils/vgg_19_train_loss.txt
+cp /scratch/b523m844/RNA_Secondary_Structure_Classification/$model/val_loss.txt ~/RNA_Secondary_Structure_Classification/utils/vgg_19_val_loss.txt
