a13b5a6e1af1b034c647037ef31ed3857c420939
Brian McClannahan <b523m844@ku.edu>
2019-12-26 12:15:18 -0600

    Slightly increased learning rate.

--- models/vgg_19.py
+++ models/vgg_19.py
@@ -4,7 +4,7 @@ from torchvision import models
 
 model_name = "vgg_19"
 
-model = Model(models.vgg19,model_name,learning_rate=.001,iteration_limit=500)
+model = Model(models.vgg19,model_name,learning_rate=.005,iteration_limit=500)
 model.build_model()
 model.train_model()
 model.test_model(iterations_to_test=[299,399,499])
