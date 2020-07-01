56b84466e26ced8a2856220799af0f33cb85e9cf
Brian McClannahan <b523m844@ku.edu>
2020-07-01 14:22:51 -0500

    Changed model to use testing folder.

--- models/multi_resnet_50.py
+++ models/multi_resnet_50.py
@@ -2,7 +2,7 @@ from multi_training_phase_model import Multi_Training_Phase_Model
 import torch.nn as nn
 from torchvision import models
 
-model_name = "multi_resnet_50"
+model_name = "testing"
 
 weights = [.8,.5]#
 
