987ffa94e28328ac00840772f50a4c9c24702f4a
Brian McClannahan <b523m844@ku.edu>
2020-09-01 16:46:20 -0500

    Added euclidean layer definition and reduced size of fc layer.

--- models/Siamese/Siamese_Network.py
+++ models/Siamese/Siamese_Network.py
@@ -1,4 +1,12 @@
+import torch
 from torch import nn
+
+class EuclidianDistance(nn.Module):
+    def __init__(self):
+        super(EuclidianDistance, self).__init__()
+
+    def forward(self, x1, x2):
+        return torch.abs(x1-x2)
 
 class SiameseNetwork(nn.Module):
     def __init__(self,model_func):
@@ -7,38 +15,35 @@ class SiameseNetwork(nn.Module):
         self.cnn1 = self.create_cnn(model_func)
 
         # Defining the fully connected layers
-        self.fc1 = nn.Sequential(
+        self.flatten = nn.Sequential(
         # First Dense Layer
         nn.Linear(30976, 1024),
-        nn.ReLU(inplace=True),
-        nn.Dropout2d(p=0.5),
-        # Second Dense Layer
-        nn.Linear(1024, 128),
-        nn.ReLU(inplace=True),
-        # Final Dense Layer
-        nn.Linear(128,2))
-
+        nn.ReLU(inplace=True))
+        self.euclidean = nn.Sequential(EuclidianDistance())
+        self.fc = nn.Sequential(
+            nn.Linear(1024,1)
+        )
     
     def create_cnn(self,model_func):
         modules=list(model_func().children())[:-1]
         cnn = nn.Sequential(*modules)
         for p in cnn.parameters():
             p.requires_grad = False
-        
         return cnn
-
 
     def forward_once(self, x):
         # Forward pass 
         output = self.cnn1(x)
         output = output.view(output.size()[0], -1)
-        output = self.fc1(output)
+        output = self.flatten(output)
         return output
 
     def forward(self, input1, input2):
         # forward pass of input 1
-        output1 = self.forward_once(input1)
+        left = self.forward_once(input1)
         # forward pass of input 2
-        output2 = self.forward_once(input2)
-        # returning the feature vectors of two inputs
-        return output1, output2
+        right = self.forward_once(input2)
+        # pass both through euclidean layer
+        euclidean = self.euclidean(left,right)
+        output = self.fc(euclidean)
+        return output
