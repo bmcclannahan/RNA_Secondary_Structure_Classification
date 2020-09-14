ff1b0a574f9d12908611070f843c24a49ac71a2b
Brian McClannahan <b523m844@ku.edu>
2020-09-14 15:22:58 -0500

    Fixed type error.

--- Image_Processing/move_rna_to_family_folder.py
+++ Image_Processing/move_rna_to_family_folder.py
@@ -8,7 +8,7 @@ images = glob.glob(data_dir+"*.jpg")
 for image in images:
     family = int(image.split('/')[-1][2:].split('_')[0])
     if not os.path.exists(data_dir+str(family)):
-        os.makedirs(data_dir+family)
+        os.makedirs(data_dir+str(family))
         #print('new folder:', family)
     new_file = image.split('/')[-1]
     print(new_file)
