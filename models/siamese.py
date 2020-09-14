73fccee5d20f58720166795b7daf15a3a2af5ec9
Brian McClannahan <b523m844@ku.edu>
2020-09-14 15:54:56 -0500

    changed data_dir

--- models/siamese.py
+++ models/siamese.py
@@ -1,4 +1,4 @@
-from model import Model
+from Siamese import Siamese_Network
 import torch.nn as nn
 from torchvision import models
 
@@ -8,7 +8,7 @@ resnet50 = models.resnet50(pretrained=True)
 
 print(model_name)
 
-model = Model(models.resnet50,model_name)
+model = Siamese_Network.SiameseNetwork(models.resnet50,model_name)
 model.build_model()
 model.train_model()
 model.test_model()
