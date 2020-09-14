30edc073b326c21743541886e4e17395824dfbfc
Brian McClannahan <b523m844@ku.edu>
2020-09-14 14:55:17 -0500

    New script to create family folders.

---
+++ Image_Processing/move_rna_to_family_folder.py
@@ -0,0 +1,15 @@
+import os, glob
+import shutil
+
+data_dir = '/data/Siamese/train/'
+
+images = glob.glob(data_dir+"*.jpg")
+
+for image in images:
+    family = int(image.split('/')[-1][2:].split('_')[0])
+    if not os.path.exists(data_dir+family):
+        #os.makedirs(data_dir+family)
+        print('new folder:', family)
+    new_file = image.split('/')[-1]
+    print(new_file)
+    #shutil.copy(image,data_dir + family + '/' + new_file)
