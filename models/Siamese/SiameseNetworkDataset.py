5a6bb333b45d6790f493c79f033806df82a54c11
Brian McClannahan <b523m844@ku.edu>
2020-09-18 14:08:48 -0500

    Forgot to add all files in last couple commits.

--- models/Siamese/SiameseNetworkDataset.py
+++ models/Siamese/SiameseNetworkDataset.py
@@ -5,8 +5,9 @@ import numpy as np
 
 class SiameseNetworkDataset(Dataset):
     
-    def __init__(self,imageFolderDataset):
+    def __init__(self,imageFolderDataset,transforms=None):
         self.imageFolderDataset = imageFolderDataset
+        self.transforms = transforms
         
     def __getitem__(self,index):
         img0_tuple = random.choice(self.imageFolderDataset.imgs)
@@ -21,8 +22,17 @@ class SiameseNetworkDataset(Dataset):
         else:
             img1_tuple = random.choice(self.imageFolderDataset.imgs)
 
-        img0 = img0_tuple[0]
-        img1 = img1_tuple[0]
+        img0_path = img0_tuple[0]
+        img1_path = img1_tuple[0]
+
+        img0 = self.loader(img0_path)
+        img1 = self.loader(img1_path)
+        print(type(img0),type(img1))
+
+        if self.transforms is not None:
+            for transform in self.transforms:
+                img0 = transform(img0)
+                img1 = transform(img1)
         
         return img0, img1, torch.from_numpy(np.array([should_get_same_class],dtype=np.float32))
     
