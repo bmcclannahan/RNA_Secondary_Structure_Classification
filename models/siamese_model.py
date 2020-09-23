df51b802da3de73914c68c66e8b1db144f6ac66d
Brian McClannahan <b523m844@ku.edu>
2020-09-23 14:39:56 -0500

    Added debug statement.

--- models/siamese_model.py
+++ models/siamese_model.py
@@ -68,7 +68,7 @@ class Siamese_Model(Model):
         print('Initializing Dataset')
 
         data_normalization = {
-            'train': transforms.Compose([transforms.ToTensor(),
+            'train': transforms.Compose([transforms.ToTensor(),transforms.Resize([224,224])
                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
             'val': transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
