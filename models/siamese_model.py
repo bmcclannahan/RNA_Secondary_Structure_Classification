4583cbeb417e8d80654b8ff9bf495c54b28f5a52
Brian McClannahan <b523m844@ku.edu>
2020-09-23 14:48:22 -0500

    Switched transforms order so the image is resized before becoming a tensor.

--- models/siamese_model.py
+++ models/siamese_model.py
@@ -68,9 +68,9 @@ class Siamese_Model(Model):
         print('Initializing Dataset')
 
         data_normalization = {
-            'train': transforms.Compose([transforms.ToTensor(),transforms.Resize([224,224]),
+            'train': transforms.Compose([transforms.Resize([224,224]),transforms.ToTensor(),
                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
-            'val': transforms.Compose([transforms.ToTensor(),transforms.Resize([224,224]),
+            'val': transforms.Compose([transforms.Resize([224,224]),transforms.ToTensor(),
                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
         }
 
