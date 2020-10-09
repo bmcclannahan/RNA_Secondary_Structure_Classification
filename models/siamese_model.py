f4a75acfe7be77a192371eb066be5aa75a6d918d
Brian McClannahan <btmcclannahan@gmail.com>
2020-10-09 01:40:31 -0500

    Upped training image count after revert fixed model training.

--- models/siamese_model.py
+++ models/siamese_model.py
@@ -13,7 +13,7 @@ class Siamese_Model(Model):
 
     data_dir = "/scratch/b523m844/RNA_Secondary_Structure_Classification/final_datasets/Siamese"
 
-    iteration_size = {'train': 320, 'val': 16000}
+    iteration_size = {'train': 32000, 'val': 16000}
 
     def __init__(self,model_func,model_name,learning_rate=0.001,lr_gamma=0.1,lr_step=10,iteration_limit=100,validation_frequency=5,logging=True,starting_weight=.5):
         super().__init__(model_func,model_name,learning_rate,lr_gamma,lr_step,iteration_limit,None,validation_frequency,logging)
