cfc96fcee6f4b46a0e93867735eb64fbc720e345
Brian McClannahan <btmcclannahan@gmail.com>
2020-10-09 01:41:23 -0500

    Increased learning rate gamma so model will train for longer.

--- models/siamese_model.py
+++ models/siamese_model.py
@@ -15,7 +15,7 @@ class Siamese_Model(Model):
 
     iteration_size = {'train': 32000, 'val': 16000}
 
-    def __init__(self,model_func,model_name,learning_rate=0.001,lr_gamma=0.1,lr_step=10,iteration_limit=100,validation_frequency=5,logging=True,starting_weight=.5):
+    def __init__(self,model_func,model_name,learning_rate=0.001,lr_gamma=0.25,lr_step=10,iteration_limit=100,validation_frequency=5,logging=True,starting_weight=.5):
         super().__init__(model_func,model_name,learning_rate,lr_gamma,lr_step,iteration_limit,None,validation_frequency,logging)
         self.starting_weights = {'train':starting_weight, 'val':0.5}
 
