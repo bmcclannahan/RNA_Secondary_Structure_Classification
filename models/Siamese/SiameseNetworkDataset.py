346f3d57a46887336a3aad7b8bf8ac239fa4f792
Brian McClannahan <btmcclannahan@gmail.com>
2020-10-12 13:44:09 -0500

    Fixed test images not being loaded and transformed.

--- models/Siamese/SiameseNetworkDataset.py
+++ models/Siamese/SiameseNetworkDataset.py
@@ -46,42 +46,45 @@ class SiameseNetworkDataset(Dataset):
                         different_images.append([image_dict[key][j],image_dict[new_key][k],1])
             self.image_list.extend(same_images)
             self.image_list.extend(different_images)
+            print("image list:",self.image_list[0:10])
 
     def __getitem__(self,index):
         if self.mode:
-            image0, image1, label = self.image_list[self.image_count]
+            img0_path, img1_path, label = self.image_list[self.image_count]
             self.image_count += 1
             if self.image_count == len(self.image_list):
                 self.image_count = 0
-            return image0, image1, torch.from_numpy(np.array(label,dtype=np.float32))
 
-        img0_tuple = random.choice(self.imageFolderDataset.imgs)
-        #we need to make sure approx 50% of images are in the same class
-        should_get_same_class = random.random()
-        if should_get_same_class < self.weight:
-            while True:
-                #keep looping till the same class image is found
-                img1_tuple = random.choice(self.imageFolderDataset.imgs) 
-                if img0_tuple[1]==img1_tuple[1]:
-                    break
         else:
-            while True:
-                #keep looping till the same class image is found
-                img1_tuple = random.choice(self.imageFolderDataset.imgs) 
-                if not img0_tuple[1]==img1_tuple[1]:
-                    break
+            img0_tuple = random.choice(self.imageFolderDataset.imgs)
+            #we need to make sure approx 50% of images are in the same class
+            should_get_same_class = random.random()
+            if should_get_same_class < self.weight:
+                while True:
+                    #keep looping till the same class image is found
+                    img1_tuple = random.choice(self.imageFolderDataset.imgs) 
+                    if img0_tuple[1]==img1_tuple[1]:
+                        break
+            else:
+                while True:
+                    #keep looping till the same class image is found
+                    img1_tuple = random.choice(self.imageFolderDataset.imgs) 
+                    if not img0_tuple[1]==img1_tuple[1]:
+                        break
 
-        img0_path = img0_tuple[0]
-        img1_path = img1_tuple[0]
+            img0_path = img0_tuple[0]
+            img1_path = img1_tuple[0]
+            label = img0_tuple[1] != img1_tuple[1]
 
         img0 = Image.open(img0_path).convert('RGB')
         img1 = Image.open(img1_path).convert('RGB')
+
         
         if self.transforms is not None:#I think the transform is essential if you want to use GPU, because you have to trans data to tensor first.
             img0 = self.transforms(img0)
             img1 = self.transforms(img1)
         
-        return img0, img1, torch.from_numpy(np.array([img0_tuple[1] != img1_tuple[1]],dtype=np.float32))
+        return img0, img1, torch.from_numpy(np.array([label],dtype=np.float32))
     
     def __len__(self):
         return len(self.imageFolderDataset.imgs)
