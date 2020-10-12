cfb07b96c185bacbee3b622fb1a0e082dc464271
Brian McClannahan <btmcclannahan@gmail.com>
2020-10-12 16:43:37 -0500

    Back to debugging why the labels are only being read as 0.

--- models/siamese_model.py
+++ models/siamese_model.py
@@ -12,8 +12,8 @@ import time
 
 class Siamese_Model(Model):
 
-    #data_dir = "/scratch/b523m844/RNA_Secondary_Structure_Classification/final_datasets/Siamese"
-    data_dir = "/data/Siamese/"
+    data_dir = "/scratch/b523m844/RNA_Secondary_Structure_Classification/final_datasets/Siamese"
+    #data_dir = "/data/Siamese/"
 
     iteration_size = {'train': 32000, 'val': 16000, 'test':32385}
 
@@ -126,7 +126,6 @@ class Siamese_Model(Model):
         class_total = list(0. for i in range(2))
         
         print(time.ctime())
-        print(int(Siamese_Model.iteration_size['test']/Model.batch_size))
 
         for _ in range(int(Siamese_Model.iteration_size['test']/Model.batch_size)):
             inputs1, inputs2, labels = next(iter(self.dataloaders['test']))
@@ -135,15 +134,15 @@ class Siamese_Model(Model):
             inputs2 = inputs2.to(self.device)
             labels = labels.to(self.device)
             labels,_ = torch.max(labels,1)
-            # print('labels:',labels)
+            print('labels:',labels)
             # print(len(labels))
             outputs = self.model(inputs1,inputs2)
             preds,_ = torch.max(outputs,1)
             preds = torch.round(preds)
-            # print('preds:',preds)
+            print('preds:',preds)
             # print(len(preds))
             c = (preds == labels).squeeze()
-            # print("c:",c)
+            print("c:",c)
             # print(len(c))
             # print(c[0].item())
                 
@@ -151,8 +150,6 @@ class Siamese_Model(Model):
                 label = int(labels[i].item())
                 class_correct[label] += c[i].item()
                 class_total[label] += 1
-        print(class_correct)
-        print(class_total)
         
         print('Model Name:', self.name)
         for i in range(2):
