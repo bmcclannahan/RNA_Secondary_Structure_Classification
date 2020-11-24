6d51be1b26513dfdad647fe53268ba2c63070b94
Brian McClannahan <btmcclannahan@gmail.com>
2020-11-24 11:37:38 -0600

    Switching network to vgg19

--- models/siamese.py
+++ models/siamese.py
@@ -6,7 +6,7 @@ model_name = "siamese"
 
 print(model_name)
 
-model = Siamese_Model(models.resnet101,model_name,logging=True)
+model = Siamese_Model(models.vgg19,model_name,logging=True)
 model.build_model()
 model.train_model()
 model.test_model(iterations_to_test=[])
