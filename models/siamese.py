1c5bf00ed960fd484f41cf947c59cbb55effd786
Brian McClannahan <btmcclannahan@gmail.com>
2020-11-24 11:31:34 -0600

    Switching siamese network to resnet101

--- models/siamese.py
+++ models/siamese.py
@@ -4,11 +4,9 @@ from siamese_model import Siamese_Model
 
 model_name = "siamese"
 
-resnet50 = models.resnet50(pretrained=True)
-
 print(model_name)
 
-model = Siamese_Model(models.resnet50,model_name,logging=True)
+model = Siamese_Model(models.resnet101,model_name,logging=True)
 model.build_model()
 model.train_model()
 model.test_model(iterations_to_test=[])
