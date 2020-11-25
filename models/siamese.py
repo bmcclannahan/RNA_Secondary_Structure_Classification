4f4bb66a79f48601c1416c813488702b619aaad5
Brian McClannahan <btmcclannahan@gmail.com>
2020-11-25 11:44:21 -0600

    Rerunning resnet 101.

--- models/siamese.py
+++ models/siamese.py
@@ -6,8 +6,8 @@ model_name = "siamese"
 
 print(model_name)
 
-model = Siamese_Model(models.resnet50,model_name,logging=True)
-#model = Siamese_Model(models.resnet101,model_name,logging=True)
+#model = Siamese_Model(models.resnet50,model_name,logging=True)
+model = Siamese_Model(models.resnet101,model_name,logging=True)
 #model = Siamese_Model(models.vgg19,model_name,logging=True)
 model.build_model()
 model.train_model()
