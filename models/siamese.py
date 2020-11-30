9733d31de7898fa6afde232b363273f1690faeb0
Brian McClannahan <btmcclannahan@gmail.com>
2020-11-30 11:52:35 -0600

    Running resnet 50 again

--- models/siamese.py
+++ models/siamese.py
@@ -6,13 +6,13 @@ model_name = "siamese"
 
 print(model_name)
 
-#model = Siamese_Model(models.resnet50,model_name,logging=True)
-model = Siamese_Model(models.resnet101,model_name,logging=True)
-model = Siamese_Model(models.vgg19,model_name,logging=True)
+model = Siamese_Model(models.resnet50,model_name,logging=True)
+#odel = Siamese_Model(models.resnet101,model_name,logging=True)
+#model = Siamese_Model(models.vgg19,model_name,logging=True)
 model.build_model()
 model.train_model()
 print("Validation image count:",model.dataloaders['val'].get_dataset_size())
 model.test_model(iterations_to_test=[])
-#print("Model func: ResNet 50")
+print("Model func: ResNet 50")
 #print("Model func: ResNet 101")
-print("Model func: VGGNet 19")
+#print("Model func: VGGNet 19")
