6ba3dde4f4f68aa97cfec7ee4a37ba0c47ae07da
Brian McClannahan <b523m844@ku.edu>
2020-12-04 16:15:33 -0600

    Fixed definitions of resnet 101.

--- models/siamese_r101.py
+++ models/siamese_r101.py
@@ -2,17 +2,13 @@ import torch.nn as nn
 from torchvision import models
 from siamese_model import Siamese_Model
 
-model_name = "siamese"
+model_name = "siamese_r101"
 
 print(model_name)
 
-#model = Siamese_Model(models.resnet50,model_name,logging=True)
-#model = Siamese_Model(models.resnet101,model_name,logging=True)
-model = Siamese_Model(models.vgg19,model_name,logging=True,model_type='vggnet')
+model = Siamese_Model(models.resnet101,model_name,logging=True)
 model.build_model()
 model.train_model()
 print("Validation image count:",model.dataloaders['val'].get_dataset_size())
 model.test_model(iterations_to_test=[])
-#print("Model func: ResNet 50")
-#print("Model func: ResNet 101")
-print("Model func: VGGNet 19")
+print("Model func: ResNet 101")
