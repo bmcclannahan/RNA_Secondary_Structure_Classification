dab78583970d434c25e29e5599c001c8b3a97974
Brian McClannahan <btmcclannahan@gmail.com>
2020-12-17 13:02:43 -0600

    Rebuilding val set.

--- Batch_Scripts/generate_training_set.bat
+++ Batch_Scripts/generate_training_set.bat
@@ -10,6 +10,6 @@
 module load MATLAB/2019a
 sshfs b523m844@deadpool.ittc.ku.edu:/data/mount_data /scratch/b523m844/RNA_Secondary_Structure_Classification/data
 cd /users/b523m844/RNA_Secondary_Structure_Classification/Image_Processing
-cp -rf *.m /home/b523m844/data/rna_classification/train
-cd /home/b523m844/data/rna_classification/train
+cp -rf *.m /home/b523m844/data/rna_classification/val
+cd /home/b523m844/data/rna_classification/val
 matlab -nodisplay -nosplash -nodesktop -r "run ('/home/b523m844/data/rna_classification/generate_data_set.m');exit;"
